8fd3bcad28869b812edde0daa6aed5a9a973c6fe
Nick <so2niko@gmail.com>
2023-09-18 23:09:49 +0300

    fix: move data_t.js to separate folder

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app.js');
 const data = require('../data.js');
-const {authors, genres, filterBooks} = require('./data_t.js')
+const {authors, genres, filterBooks} = require('./data/data_t.js')
 
 console.log('Tests are started.');
 
